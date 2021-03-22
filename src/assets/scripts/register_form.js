b0d11cb391ddf112389e1997542362305e086e8c
Toni Panacek <toni.panacek@gmail.com>
2021-03-22 15:10:44 +0100

    update register form

--- src/assets/scripts/register_form.js
+++ src/assets/scripts/register_form.js
@@ -1,23 +1,23 @@
 const handleRegisterFormSubmission = () => {
   const registerForm = document.querySelector(".form-register");
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    let formData = new FormData(registerForm[0])
-    fetch('/', {
-      method: 'POST',
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString()
-    }).then(() => console.log('Form successfully submitted')).catch((error) =>
-      alert(error))
-  }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   let formData = new FormData(registerForm[0])
+  //   fetch('/', {
+  //     method: 'POST',
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: new URLSearchParams(formData).toString()
+  //   }).then(() => console.log('Form successfully submitted')).catch((error) =>
+  //     alert(error))
+  // }
 
   if (registerForm) {
     console.log('hello toni')
     setTimeout(() => {
       document.querySelector('.register').classList.remove('transparent');
     }, 7000)
-    registerForm.addEventListener("submit", handleSubmit);
+    // registerForm.addEventListener("submit", handleSubmit);
   }
 }
 
