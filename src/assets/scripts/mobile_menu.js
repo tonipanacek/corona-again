252138c30f4889ba684b8876c71df3966fcc7ff4
Toni Panacek <toni.panacek@gmail.com>
2021-04-13 09:12:55 +0200

    add no scroll to mobile menu:

--- src/assets/scripts/mobile_menu.js
+++ src/assets/scripts/mobile_menu.js
@@ -1,13 +1,16 @@
 const toggleMobileMenu = () => {
+  const body = document.body;
   const menuBtn = document.querySelector('#mobile-menu');
   const closeBtn = document.querySelector('#close-menu');
   const mobileMenu = document.querySelector('.mobile-nav-items');
 
   if (menuBtn) {
     menuBtn.addEventListener('click', () => {
+      body.classList.add('noscroll');
       mobileMenu.classList.remove('hidden');
     })
     closeBtn.addEventListener('click', () => {
+      body.classList.remove('noscroll');
       mobileMenu.classList.add('hidden');
     })
   }
